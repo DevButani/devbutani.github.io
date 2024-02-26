e5b80c8223e14df0037b9da0fcd49db7f0124f90
DevButani <devbutani26@gmail.com>
2024-02-26 23:38:02 +0530

    made links open in new tab

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,7 +8,7 @@ function About() {
           <h3>I am a Dual Degree (BTech + MTech) student in CSE Department in IIT Kharagpur.</h3>
           <h3>Passionate about fields involving solving a problem through innovative techniques.</h3>
           <h3>Coding experience in programming languages like C, C++, Python and Javascript </h3>
-          <h3> as well as with libraries/frameworks like ReactJS, Django, NumPy and Pandas.</h3>
+          <h3> as well as with libraries/frameworks like ReactJS, Bootstrap, NumPy, Tkinter and Pandas.</h3>
           <h3>My hobbies are competitive programming and playing table tennis and badminton.</h3>
         </div>
     );

e2913f89c221b0d0d96e7f42354080708b2dc95c
DevButani <devbutani26@gmail.com>
2024-02-28 17:28:44 +0530

    made contact info list responsive

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -9,7 +9,7 @@ function Contact() {
         <div className="container-fluid Contact">
           <h1>Contact Me</h1>
           <br/>
-          <ul>
+          <ul className="container-fluid">
             <li><img src={GMLogo} alt="GMail logo"/><a target='_blank' rel="noreferrer" className="link" href="mailto:devbutani26@gmail.com">devbutani26@gmail.com</a></li>
             <li><img src={LILogo} alt="LinkedIn logo"/><a target='_blank' rel="noreferrer" className="link" href="https://www.linkedin.com/in/dev-butani-b570212b7">Dev Butani</a></li>
             <li><img src={GHLogo} alt="Github logo"/><a target='_blank' rel="noreferrer" className="link" href="https://github.com/DevButani">Github</a></li>
